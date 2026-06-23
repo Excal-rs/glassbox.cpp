7771e250195d30e4a9d2151eb847ee286a1fbaea
excal.rs <excal.rs@protonmail.com>
2026-06-23 01:25:37 +0100

    Added Query, Key, Value matrix creation

--- src/attention.cpp
+++ src/attention.cpp
@@ -26,16 +26,44 @@ static Tensor output_projection(const Tensor& concat, const Attention& attn);
 Tensor attention(const Tensor& ids, const LayerNorm& ln_1, const Attention& attn, const Config& config)
 {
     Tensor xn { layernorm(ids, ln_1, config.ln_eps) };
-
     return {};
 }
 
 
 // --------- Helper Function Definitions ---------
 
-static std::array<Tensor, 3> qkv_projection(const Tensor& xn, const Attention& attn)
+// Extract and Create the Query, Key and Value Matrices from the attention block
+static std::array<Tensor, 3> qkv_projection(const Tensor& xn, const Attention& attn, const Config& config)
 {
-    return {};
+    const size_t seq      = xn.shape[0];
+    const size_t n_embd   = config.n_embd;
+    const size_t n_stride = n_embd * 3;
+
+    // Perform (xn . w) + b
+    Tensor n { matmul(xn, attn.c_attn.w) };
+    for (size_t i = 0; i < n.data.size(); ++i){
+        n.data[i] += attn.c_attn.b.data[i % n_stride];
+    }
+
+    // [Q, K, V]
+    std::array<Tensor, 3> qkv {};
+    for (Tensor& t : qkv){
+        t.shape = {seq, n_embd};
+        t.data  = std::vector<float>(seq * n_embd); 
+    }
+
+    // Extract data
+    for (size_t t = 0; t < seq; ++t){
+        for (size_t c = 0; c < n_embd; ++c){
+            const size_t dst = t * n_embd + c;
+            const size_t src = t * n_stride + c;
+            qkv[0].data.at(dst) = n.data[src];
+            qkv[1].data.at(dst) = n.data[src + n_embd];
+            qkv[2].data.at(dst) = n.data[src + n_embd * 2];
+        }
+    }
+
+    return qkv;
 }
 
 static Tensor split_heads(const Tensor& m, size_t n_head)
