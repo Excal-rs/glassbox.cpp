4cdd794ee38ac7a6c8a1597dfedbec7d10243c45
excal.rs <excal.rs@protonmail.com>
2026-09-18 18:33:36 +0100

    Refactored layernorm.h/cpp

--- include/glassbox/layernorm.h
+++ include/glassbox/layernorm.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Include `glassbox` libraries
 #include "glassbox/model.h"
 
 // --------- Public API ---------
--- src/layernorm.cpp
+++ src/layernorm.cpp
@@ -1,7 +1,10 @@
-#include <cmath>
+// Include `glassbox` libraries
 #include "glassbox/layernorm.h"
 
-// --------- Static Forward Declarations
+// Include stdlib
+#include <cmath>
+
+// --------- Static Forward Declarations ---------
 static inline void layernorm_vector(const float* in, float* out, size_t dim, const LayerNorm& ln, float eps);
 
 
@@ -10,16 +13,16 @@ static inline void layernorm_vector(const float* in, float* out, size_t dim, con
 // Takes in a Tensor of token vectors (shape {seq, n_embd}) and a LayerNorm,
 // Will return a new Tensor of the same shape with each row normalised independently
 Tensor layernorm(const Tensor& x, const LayerNorm& ln, float eps){
-    const size_t seq = x.shape[0];
-    const size_t dim = x.shape[1];
+    const size_t seq { x.shape[0] };
+    const size_t dim { x.shape[1] };
 
     Tensor out {
         .data  = std::vector<float>(x.data.size()),
         .shape = x.shape
     };
 
-    for (size_t r = 0; r < seq; ++r){
-        const size_t base = r * dim;
+    for (size_t r {0}; r < seq; ++r){
+        const size_t base { r * dim };
         layernorm_vector(&x.data[base], &out.data[base], dim, ln, eps);
     }
     return out;
@@ -33,16 +36,16 @@ Tensor layernorm(const Tensor& x, const LayerNorm& ln, float eps){
 // then each element is scaled and shifted by the learned weight/bias.
 static inline void layernorm_vector(const float* in, float* out, size_t dim, const LayerNorm& ln, float eps)
 {
-    float sum    = 0;
-    float sum_sq = 0;
-    for (size_t i = 0; i < dim; ++i){
+    float sum    {0};
+    float sum_sq {0};
+    for (size_t i {0}; i < dim; ++i){
         sum    += in[i];
         sum_sq += in[i] * in[i];
     }
-    float mean     =  sum / dim;
-    float variance = (sum_sq / dim) - (mean * mean);
+    float mean     { sum / dim };
+    float variance { (sum_sq / dim) - (mean * mean) };
 
-    for (size_t i = 0; i < dim; ++i){
+    for (size_t i {0}; i < dim; ++i){
         out[i] = (in[i] - mean) / std::sqrt(variance + eps)
                  * ln.weight.data[i]
                  + ln.bias.data[i];
@@ -57,4 +60,3 @@ static inline void layernorm_vector(const float* in, float* out, size_t dim, con
 //   output    y_i   = x_hat_i * weight_i + bias_i
 //
 // eps is added inside the sqrt for numerical stability.
-
